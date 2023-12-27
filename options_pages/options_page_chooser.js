80b15899b5ba3543db18a27d6be9cfd6b01361f8
Shustov Dmitriy <dshustov@web-bee.ru>
2023-12-27 22:42:04 +0300

    Initial commit

---
+++ options_pages/options_page_chooser.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function () {
+  setPageCooserEvents();
+});
+
+//Set Events
+function setPageCooserEvents() {
+  $(".chooser").click(function () {
+    var panel = $(this).attr("id");
+    if ($(this).hasClass("selected")) return;
+    var id = $(this).attr("id");
+    if (id == "getting_started") {
+      openExtPage("http://www.editthiscookie.com/start/");
+      return;
+    }
+    ls.set("option_panel", panel);
+    location.href = "/options_pages/" + id + ".html";
+  });
+}
+
+function openExtPage(url) {
+  chrome.tabs.getCurrent(function (tab) {
+    chrome.tabs.create({
+      index: tab.index + 1,
+      url: url,
+      active: true,
+      openerTabId: tab.id,
+    });
+  });
+}
